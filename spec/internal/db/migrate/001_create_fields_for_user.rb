58769ced16ac1cd5a59e60bfadc69931365fb2da
Geoff Hodgson <geoffroh@gmail.com>
2012-07-04 22:22:00 +1000

    Bug in migration after merge.

--- spec/internal/db/migrate/001_create_fields_for_user.rb
+++ spec/internal/db/migrate/001_create_fields_for_user.rb
@@ -16,18 +16,18 @@ class CreateFieldsForUser < ActiveRecord::Migration
       t.date     :date_value
       t.timestamps
     end
-    add_index :fields_attributes, ["user_id", "field_id"], :unique => true
-    add_index :fields_attributes, :user_id
-    add_index :fields_attributes, :field_id
-    add_index :fields_attributes, :string_value
-    add_index :fields_attributes, :boolean_value
-    add_index :fields_attributes, :date_value        
+    add_index :field_attributes, ["user_id", "field_id"], :unique => true
+    add_index :field_attributes, :user_id
+    add_index :field_attributes, :field_id
+    add_index :field_attributes, :string_value
+    add_index :field_attributes, :boolean_value
+    add_index :field_attributes, :date_value        
     create_table(:field_select_options) do |t|
       t.string :option, :null => false, :limit => 63
       t.integer :field_id
       t.timestamps
     end
-    add_index :fields_select_options, :field_id, :unique => false
+    add_index :field_select_options, :field_id, :unique => false
 
   end
 
