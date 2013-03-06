992f31a22497b5ef8a7d3a1f7ec9bd288d87af6c
wanglian <wanglian1024@gmail.com>
2013-03-06 15:43:30 +0800

    fix uninitialized constant HasFields

--- lib/has_fields/engine.rb
+++ lib/has_fields/engine.rb
@@ -1,10 +1,12 @@
-class HasFields::Engine < Rails::Engine
-  isolate_namespace HasFields
+module HasFields
+  class Engine < Rails::Engine
+    isolate_namespace HasFields
   
-  config.to_prepare do
-    unless ActiveRecord::Base.included_modules.include?(HasFields::InstanceMethods)
-      ActiveRecord::Base.extend HasFields::ClassMethods
-      ActiveRecord::Base.send :include, HasFields::InstanceMethods
+    config.to_prepare do
+      unless ActiveRecord::Base.included_modules.include?(HasFields::InstanceMethods)
+        ActiveRecord::Base.extend HasFields::ClassMethods
+        ActiveRecord::Base.send :include, HasFields::InstanceMethods
+      end
     end
   end
 end
